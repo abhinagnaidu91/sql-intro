ccb07aab61ed80389836cffafae2d7a3d57acec0
Abhinag Naidu <abhinag.naidu@kellogg.northwestern.edu>
2024-01-11 03:16:02 +0000

    SQL exercise sections 2-4

--- lab-4-4.sql
+++ lab-4-4.sql
@@ -2,6 +2,12 @@
 -- NOTE: need more advanced SQL to answer this question without
 --       raising a warning: "Field of aggregated query neither grouped nor aggregated"
 
+SELECT teams.name, players.first_name, players.last_name, max(stats.home_runs) FROM players
+INNER JOIN stats ON stats.player_id = players.id
+INNER JOIN teams ON stats.team_id = teams.id
+WHERE teams.year = "2019"
+GROUP BY teams.name;
+
 -- Expected result:
 --
 -- +-------------------------------+------------+-------------+----------------------+
